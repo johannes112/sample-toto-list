742dea0876f73e6026410dcfcc131766ab2c9692
johannes112 <johannes.launer@emmos.de>
2020-02-19 09:09:56 +0100

    Updates

---
+++ precache-manifest.d5002a327f2ba7628e85029afe60931b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a56fa7896537a9eff4d302867cc6ef",
+    "url": "/sample-toto-list/index.html"
+  },
+  {
+    "revision": "0105171b785ddf876d02",
+    "url": "/sample-toto-list/static/css/main.69546fa5.chunk.css"
+  },
+  {
+    "revision": "305df936bc3f9b4452d4",
+    "url": "/sample-toto-list/static/js/2.bded0d1f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sample-toto-list/static/js/2.bded0d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0105171b785ddf876d02",
+    "url": "/sample-toto-list/static/js/main.f60fee74.chunk.js"
+  },
+  {
+    "revision": "f194c7b300cdd66dffb3",
+    "url": "/sample-toto-list/static/js/runtime-main.0dc050cb.js"
+  }
+]);
